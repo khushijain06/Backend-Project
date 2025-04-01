e221a72254ae45f2b7370389f39e645e96f806a3
khushijain06 <codingwithkhushi@gmail.com>
2025-04-01 11:00:30 +0530

    setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { error } from 'console';
+import { response } from 'express';
+import fs from 'fs'
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    });
+    const uploadCloudinary= async (localpath) =>{
+        try{
+      if(!localpath) return null;
+       const uploadResult =   await cloudinary.uploader
+       .upload(
+           localpath, {
+               resource_type: 'auto',
+           })
+          fs.unlinkSync(localpath) 
+        console.log("file uploaded",uploadResult.url)
+       return response
+        }
+        catch(error){
+            fs.unlinkSync(localpath) //remove local saved temp file   
+            console.log("error file upload" , error);
+            return null;
+        }}
+      
+        export {uploadCloudinary}
+    // Upload an image
+     
